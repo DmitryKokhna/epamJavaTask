3fceb82e0ff6e631502cc6bfedbce0f562fb36b1
Dmitry Kokhna from consol <dmitry.kokhna@gmail.com>
2021-05-25 22:03:26 +0300

    Task 1 Main

---
+++ src/main/java/org/epamJava/Solution.java
@@ -0,0 +1,41 @@
+package org.epamJava;
+
+import java.util.Scanner;
+
+/**
+ * Задача 1 - Main
+ *    Приветствовать любого пользователя при вводе его имени через командную строку.
+ */
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+
+        outHello();
+
+    }
+
+    public static void outHello() throws InterruptedException {
+        Scanner scannerForName=new Scanner(System.in);
+        System.out.println("Введите Ваше имя:");
+
+        String name = scannerForName.nextLine();
+
+        if(!name.isEmpty()){
+            String str="Добро пожаловать ";
+            System.out.print(str);
+
+            /**
+             * Просто делаем красиво
+             */
+            for (int i=0;i<3;i++){
+                System.out.print('.');
+                Thread.sleep(500);
+            }
+
+            System.out.print(" " + name);
+        }
+        else {
+            System.out.println("Имя не введено");
+        }
+
+    }
+}
---
+++ src/test/java/org/epamJava/SolutionTest.java
@@ -0,0 +1,19 @@
+package org.epamJava;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SolutionTest extends TestCase {
+    @Test
+    public void testOutHello() {
+
+                  assertTrue( true);
+            //    System.out.println("TeSt OK");
+
+        }
+
+        @Test
+        public void testMain() {
+            assertTrue( true);
+        }
+    }
