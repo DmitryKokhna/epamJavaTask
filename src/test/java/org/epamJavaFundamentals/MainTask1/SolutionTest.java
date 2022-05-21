b2ae6eda15756725cfa65358827c62317129ae62
Dmitry Kokhna from consol <dmitry.kokhna@gmail.com>
2022-05-21 10:48:33 +0300

    testOutHello assertTrue true- set s TRUE

--- src/test/java/org/epamJavaFundamentals/MainTask1/SolutionTest.java
+++ src/test/java/org/epamJavaFundamentals/MainTask1/SolutionTest.java
@@ -7,7 +7,7 @@ public class SolutionTest extends TestCase {
     @Test
     public void testOutHello() {
 
-                  assertTrue( true);
+                  assertTrue(true);
               System.out.println("TeSt1 OK");
 
         }
